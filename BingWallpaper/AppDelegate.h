c64ebff90e91858c7050ff3e1af83da1becf3909
xia0guang <wuxiaoguang@xiaoguang-3.local>
2014-08-18 17:16:28 -0700

    Initial Commit

---
+++ BingWallpaper/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  BingWallpaper
+//
+//  Created by xiaoguang on 8/18/14.
+//  Copyright (c) 2014 com.Xiaoguang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ BingWallpaper/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BingWallpaper
+//
+//  Created by xiaoguang on 8/18/14.
+//  Copyright (c) 2014 com.Xiaoguang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
